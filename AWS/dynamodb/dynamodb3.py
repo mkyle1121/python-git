c3e94e929662fd0c8477116eab61ea1930808b72
Mike <mkyle1121@gmail.com>
2018-08-11 23:53:23 -0700

    2018-8-11

--- AWS/dynamodb/dynamodb3.py
+++ AWS/dynamodb/dynamodb3.py
@@ -1,8 +1,9 @@
 import boto3
 import uuid
 
-client = boto3.client('dynamodb')
+client = boto3.resource('dynamodb')
+table = client.Table('users')
 
 item = {'id':uuid.uuid4().hex, 'color': 'red', 'size': 'large'}
 
-client.put_item(TableName='miketable', Item={'string' : {'id':7, 'S' : 'string'}})
+table.put_item(Item=item)
